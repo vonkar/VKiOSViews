08274474c1c6266eabe4888dbdb77f51ef3b8fcc
Vinod Onkar <vinodonkar@Vinods-MacBook-Pro.local>
2017-09-02 23:01:53 +0530

    Initia commit

---
+++ Example/Pods/Target Support Files/Pods-VKiOSViews_Example/Pods-VKiOSViews_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VKiOSViews_Example : NSObject
+@end
+@implementation PodsDummy_Pods_VKiOSViews_Example
+@end
---
+++ Example/Pods/Target Support Files/VKiOSViews/VKiOSViews-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_VKiOSViews : NSObject
+@end
+@implementation PodsDummy_VKiOSViews
+@end
